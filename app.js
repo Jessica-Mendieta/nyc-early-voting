aa1138d6d14146327bdfe45aa396cd89ad7cd782
Jessica Mendieta <62675825+Jessica-Mendieta@users.noreply.github.com>
2020-09-30 23:46:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -432,8 +432,8 @@ map.on("load", function () {
                     "data": geojsonData
                 },
                 "paint": {
-                    "circle-radius": 5, // size of circles
-                    "circle-color": "#3D2E5D", // color of circles
+                    "circle-radius": 6, // size of circles
+                    "circle-color": "#D91E0D", // color of circles
                     "circle-stroke-color": "white",
                     "circle-stroke-width": 1,
                     "circle-opacity": 0.7
