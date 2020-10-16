e3b20c51f09f525e16bb643083947242ce316ace
Jessica Mendieta <62675825+Jessica-Mendieta@users.noreply.github.com>
2020-10-15 22:26:53 -0400

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ const config = {
     center: [-73.935242, 40.730610], //Lng, Lat
     zoom: 10, //Default zoom
     title: "Early Voting Poll Sites in NYC",
-    description: "Early voting runs from Saturday, October 24 to Sunday, November 1. Type an address to sort the list of poll sites by distance, or filter by borough, and [verify your poll site](findmypollsite.vote.nyc)",
+    description: "Early voting runs from Saturday, October 24 to Sunday, November 1. Type an address to sort the list of poll sites by distance, or filter by borough, and verify your poll site at findmypollsite.vote.nyc",
     sideBarInfo: ["Name", "Address", "Zip Code"],
     popupInfo: ["Name"],
     filters: [
